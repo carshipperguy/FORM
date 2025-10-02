1f6d6f761ee4ff3f58f2bfafff034ec16f705399
Trevor Hawkins <trevorhawkins@dsignable.com>
2025-10-02 14:08:14 -0600

    chore(debug): add Dsignable console marker to verify latest Form build

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,4 +4,8 @@ import "./index.css";
 
 // Attribution tracking is handled silently by the attribution-tracker module
 
+// Dsignable deploy marker - helps verify latest version in the browser console
+// If you see this line, the Form app is on the current branch build
+console.log("[Dsignable] Form build active - fix/meta-pixel-dedup");
+
 createRoot(document.getElementById("root")!).render(<App />);
